0c09a1b0ec15655e0aa03ecbc457b3b4cad7ef85
Abhishek Basak <Abhishek.Basak@Pearson.com>
2018-03-05 10:14:52 -0600

    JBoss Deployment

--- src/main/java/com/basak/busket/BusketApplication.java
+++ src/main/java/com/basak/busket/BusketApplication.java
@@ -2,11 +2,20 @@ package com.basak.busket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BusketApplication {
+public class BusketApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusketApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<BusketApplication> applicationClass = BusketApplication.class;
 }
