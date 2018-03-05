9951a9220b3d6df451a92462ae7fe0612767bdad
Abhishek Basak <Abhishek.Basak@Pearson.com>
2018-03-05 09:23:25 -0600

    UI App Integration

--- src/main/java/com/basak/busket/controller/BusketAPIController.java
+++ src/main/java/com/basak/busket/controller/BusketAPIController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import com.basak.busket.model.Item;
 import com.basak.busket.service.ItemService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class BusketAPIController {
 
 	@Autowired
